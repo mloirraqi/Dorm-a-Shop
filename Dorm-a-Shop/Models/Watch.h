9664607ef2b0edaa57ee05890bd7506763c4b64c
Ilana Shapiro <ilanashapiro@fb.com>
2019-07-16 15:58:28 -0700

    worked on watch functionality for posts and added properties to post object

---
+++ Dorm-a-Shop/Models/Watch.h
@@ -0,0 +1,17 @@
+//
+//  Watch.h
+//  Dorm-a-Shop
+//
+//  Created by ilanashapiro on 7/16/19.
+//  Copyright © 2019 ilanashapiro. All rights reserved.
+//
+
+#import <Parse/Parse.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Watch : PFObject
+
+@end
+
+NS_ASSUME_NONNULL_END
